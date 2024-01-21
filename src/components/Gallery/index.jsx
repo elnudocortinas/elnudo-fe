89f0dad8ce5d05a48661a6b96accb015bc397b65
Manu <manuvaglz@gmail.com>
2024-01-21 14:49:01 -0600

    K-0001 feat: Finish translations

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -22,7 +22,7 @@ const Gallery = () => {
   return(
     <section id='services'>
       <header className="max-w-7xl mx-auto flex flex-col md:flex-row justify-end md:justify-between items-center md:items-end">
-        <h2 className="text-subtitle max-w-[20ch]">Nuestros <br /> <span className="text-secondary uppercase text-custom ">Servicios <br />y Productos</span></h2>
+        <h2 className="text-subtitle max-w-[20ch]">{currentLang.locale.gallery.title} <br /> <span className="text-secondary uppercase text-custom ">{currentLang.locale.gallery.subtitle1} <br />{currentLang.locale.gallery.subtitle2}</span></h2>
         <ul className="flex flex-col md:flex-row w-full md:w-auto justify-between gap-[1px] text-center">
           {services && services.map(service => {
             const { id, name} = service
